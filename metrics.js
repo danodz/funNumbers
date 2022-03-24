5c4e021e092d7e8d82fc6d6d0a786a2ba4eb13b9
Erwan <erwan.bourdeau@gmail.com>
2022-03-23 23:30:03 -0400

    Implemented save/load and did the necessary refactoring. More refactorin/cleanup could be good

--- metrics.js
+++ metrics.js
@@ -1,5 +1,5 @@
 var increment = {min:1,max:10};
-var updateSpeed = {min:1000,max:5000};
+var updateSpeed = {min:1,max:5};
 
 var fontChance = {min:1,max:10};
 var fontSize = {min:15,max:45};
@@ -7,7 +7,7 @@ var fontSize = {min:15,max:45};
 var colorChance = {min:1,max:10};
 
 var incrementSpeedModifier= 1;
-var newNumber= {min: 3000,max: 4000};
+var newNumber= {min: 3,max: 4};
 
 var rotateCloseToFull = {min: 350,max: 380};
 
@@ -16,8 +16,7 @@ var pressMsgSpeed = 0.05;
 var progression = {
     5: function(){
         incrementSpeedModifier= 0.75,
-        newNumber= {min: 20000,max: 30000}
-        console.log(5);
+        newNumber= {min: 20,max: 30}
         if(showMessage)
         {
             tryPressNumber();
@@ -25,29 +24,26 @@ var progression = {
     },
     10: function(){
         incrementSpeedModifier= 0.5,
-        newNumber= {min: 20000,max: 30000}
-        console.log(10);
+        newNumber= {min: 20,max: 30}
     },
     15: function(){
         incrementSpeedModifier= 0.25,
-        newNumber= {min: 30000,max: 60000}
+        newNumber= {min: 30,max: 60}
         clickFunctions.remove.weight=1;
         clickFunctions.nothing.weight=1;
-        console.log(15);
     }
 };
 
 var clickFunctions = {
-    rotateQuarter: { weight: 1, fn: function(e, element){
+    rotateQuarter: {name: "rotateQuarter", weight: 1, fn: function(e, element){
         rotateBy(e.target, 45);
-        console.log(element)
     }},
-    rotateConstant: { weight: 1, fn: function(e, element){
+    rotateConstant: {name: "rotateConstant", weight: 1, fn: function(e, element){
         setInterval(function(){
             rotateBy(e.target, 1);
         }, 1);
     }},
-    spin: { weight: 1, fn: function(e, element){
+    spin: {name: "spin", weight: 1, fn: function(e, element){
         var i = 0;
         var interval = setInterval(function(){
             i++
@@ -56,7 +52,7 @@ var clickFunctions = {
                 clearInterval(interval);
         }, 1);
     }},
-    spinAlmost: { weight: 1, fn: function(e, element){
+    spinAlmost: {name: "spinAlmost", weight: 1, fn: function(e, element){
         var i = 0;
         var interval = setInterval(function(){
             i++
@@ -65,28 +61,28 @@ var clickFunctions = {
                 clearInterval(interval);
         }, 1);
     }},
-    biggerFont: { weight: 1, fn: function(e, element){
+    biggerFont: {name: "biggerFont", weight: 1, fn: function(e, element){
         var oldSize = window.getComputedStyle(e.target, null).getPropertyValue('font-size').split("px")[0]
         
         e.target.style.fontSize = (parseInt(oldSize) + 1) + "px";
     }},
-    smallerFont: { weight: 1, fn: function(e, element){
+    smallerFont: {name: "smallerFont", weight: 1, fn: function(e, element){
         var oldSize = window.getComputedStyle(e.target, null).getPropertyValue('font-size').split("px")[0]
         
         e.target.style.fontSize = (parseInt(oldSize) - 1) + "px";
     }},
-    moveRight: { weight: 1, fn: function(e, element){
+    moveRight: {name: "moveRight", weight: 1, fn: function(e, element){
         var oldLeft = e.target.style.left.split("px")[0];
         e.target.style.left = (parseInt(oldLeft) + 10) + "px";
     }},
-    moveLeft: { weight: 1, fn: function(e, element){
+    moveLeft: {name: "moveLeft", weight: 1, fn: function(e, element){
         var oldLeft = e.target.style.left.split("px")[0];
         e.target.style.left = (parseInt(oldLeft) - 10) + "px";
     }},
-    remove: { weight: 0, fn: function(e, element){
+    remove: {name: "remove", weight: 0, fn: function(e, element){
         e.target.remove()
     }},
-    nothing: { weight: 0, fn: function(e, element){
+    nothing: {name: "nothing", weight: 0, fn: function(e, element){
     }},
 };
 
